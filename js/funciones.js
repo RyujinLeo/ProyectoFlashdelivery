11c0a1effd7dff24cf5325a834fb46bda474a164
RyujinLeo <allan321.h@gmail.com>
2023-07-24 00:24:44 -0600

    login terminado

--- js/funciones.js
+++ js/funciones.js
@@ -2,3 +2,15 @@ function redirland() {
     location.href= "/html/logi.html"
 }
 
+function redirregmt() {
+    location.href= "/html/Registromotorista.html"
+}
+function redirregcl() {
+    location.href= "/html/Registrocliente.html"
+}
+function rediriniciocl() {
+    location.href= "/html/indexcliente.html"
+}
+function rediriniciomt() {
+    location.href= "/html/Indexmotorista.html"
+}
