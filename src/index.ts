66b656fe9bc186b42c17486a9e735593e17a4e4d
IdaIvZe <idalia.zelaya@unah.hn>
2023-08-24 22:35:37 -0600

    backend

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response, Express } from 'express';
+import { Database } from './utils/database';
+import cors from 'cors';
+
+const app:Express = express();
+const db:Database = new Database();
+app.use(cors());
+app.use(express.json());
+
+
+app.get("/", (req:Request, res:Response) => {
+    res.send("Servidor para el ejercicio de teams");
+    res.end();
+});
+
+app.listen(3001, () => {
+    console.log("Servidor levantado en el puerto 3001");
+});
